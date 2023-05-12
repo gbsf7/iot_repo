4c8902579eec49d9a3a252eb2499d29b2ea0c523
gbsf7 <3332200014@untirta.ac.id>
2023-05-12 21:48:19 +0700

    first commit

---
+++ resources/views/pasien/dataview.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Data Sensor</title>
+</head>
+
+<body>
+    <h2> Data suhu dan kelembaban</h2>
+    <table>
+        <tr>
+            <th>No.</th>
+            <th>Suhu</th>
+            <th>Kelembaban</th>
+            <th>Aksi</th>
+        </tr>
+        <tr>
+            <td>
+                1
+            </td>
+            <td>
+                2
+            </td>
+            <td>
+                3
+            </td>
+            <td>
+                4
+            </td>
+        </tr>
+    </table>
+</body>
